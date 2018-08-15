abcfe7f5ef899214fcf2d67b1dc23eb370efc73b
zecollokaris <collo.kariss@gmail.com>
2018-08-15 13:13:42 +0300

    done with image slider

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
 
 var slides = $('.slide');
 
+var flag = 0;
+
 slides.first().before(slides.last());
 
 setInterval(show,4000);
@@ -15,7 +17,16 @@ function show(){
     slides = $('.slide');
     var activeSlide = $('.active');
 
-    slide.last().after(slides.first());
+    slides.last().after(slides.first());
 
     activeSlide.removeClass('active').next('.slide').addClass('active');
+
+    if (flag==0){
+        $(".box").css({'-webkit-clip-path':"polygon(0% 100%,100% 100%,100% 90%,85% 95%,10% 78%,7% 11%,90% 5%,85% 95%,100% 95%, 100% 0%,0% 0%,0% 100%)"});
+        flag = 1;
+    }
+    else{
+        $(".box").css({ '-webkit-clip-path':"polygon(0% 100%, 100% 100%,100% 80%,93% 85%,8% 95%, 15% 6%,89% 22%,93% 85%,100% 80%,100% 0%,0% 0%,0% 100%)"});
+        flag=0;
+    }
 }
