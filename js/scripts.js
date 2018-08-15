165665e9bab6be4a5fefd0b2a115fbde9c1e22e3
zecollokaris <collo.kariss@gmail.com>
2018-08-15 11:59:59 +0300

    adding slider

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ $(document).ready(function () {
         $('nav ul').toggleClass('show');
     });
 });
+
+
+var slides = $('.slide');
+
+slides.first().before(slides.last());
+
+setInterval(show,4000);
+
+function show(){
+    slides = $('.slide');
+    var activeSlide = $('.active');
+
+    slide.last().after(slides.first());
+
+    activeSlide.removeClass('active').next('.slide').addClass('active');
+}
