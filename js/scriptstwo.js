67a7632a7a97e7f647deeba04bdea4ae1b1da118
zecollokaris <collo.kariss@gmail.com>
2018-12-16 21:06:16 +0300

    removed images in tech folder as they are not being used

--- js/scriptstwo.js
+++ js/scriptstwo.js
@@ -5,7 +5,10 @@ $(document).ready(function () {
     });
 });
 
+// COLLAPSE NAV_BAR
 
+
+// PROJECT SECTION JAVA SCRIPT
 var mySlider = {
 
     config: {
@@ -392,3 +395,47 @@ $(document).ready(function () {
     $(window).on('resize', fn);
 
 });
+
+// COLLAPSE PROJECT SECTION
+
+
+/* FONT WRITING WORD `CODE` */
+$(function () {
+
+    // Rotate Code text
+    var $badges = $(".skillerimage");
+
+    function rotateBadge(passedBadge, speed) {
+        var rotateSpeed = speed / 180,
+            current = 0;
+
+        function badgeRotation(passedBadge) {
+            current = (current == 90) ? 271 : current + 1;
+
+            passedBadge.css({
+                "-webkit-transform": "rotate3d(0,1,0," + current + "deg)",
+                "-moz-transform": "rotate3d(0,1,0," + current + "deg)",
+                "transform": "rotate3d(0,1,0," + current + "deg)"
+            });
+
+            if (current == 360) {
+                clearInterval(rotateInterval);
+                passedBadge.removeAttr('style').removeClass('spinning');
+            }
+        }
+
+        var rotateInterval = setInterval(function () { badgeRotation(passedBadge) }, rotateSpeed);
+    }
+
+    $badges.mouseenter(function () {
+        if ($(this).hasClass('spinning')) {
+            return false;
+        } else {
+            rotateBadge($(this), 500);
+            $(this).addClass('spinning');
+        }
+    });
+
+});
+
+/*COLLAPSE FONT WRITING WORD `CODE` */
