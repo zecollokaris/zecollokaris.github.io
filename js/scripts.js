85a7d3b736acbc4c0229579175a197ac99ec7aa2
zecollokaris <collo.kariss@gmail.com>
2018-12-18 12:50:56 +0300

    moved javascript to html

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     });
 });
 
+
 // CUROSEL FOR IMAGE SLIDER
 var slides = $('.slide');
 
@@ -31,27 +32,5 @@ function show(){
         flag=0;
     }
 }
-
-// MOUSE SLIDER WITH MY SKILLS INFO
-document.addEventListener('DOMContentLoaded', function(){
-  let wrapper = document.getElementById('wrapper');
-  let topLayer = wrapper.querySelector('.top');
-  let handle = wrapper.querySelector('.handle');
-  let skew = 0;
-  let delta = 0;
-
-  if(wrapper.className.indexOf('skewed') != -1){
-    skew = 1000;
-  }
-  
-  wrapper.addEventListener('mousemove', function(e){
-    delta = (e.clientX - window.innerWidth / 2) * 0.5;
-  
-    handle.style.left = e.clientX + delta + 'px';
-
-    topLayer.style.width= e.clientX + skew + delta + 'px';
-  });
-});
-
 
 
